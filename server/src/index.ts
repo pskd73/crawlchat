66906614d8b96c574d66aaf24b6066e79ee1daf0
Pramod <pramodkumar.damam73@gmail.com>
2025-10-10 14:28:56 +0530

    Fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -635,7 +635,7 @@ app.post("/answer/:scrapeId", authenticate, async (req, res) => {
   wsRateLimiter.check();
 
   const scrape = await prisma.scrape.findFirst({
-    where: { id: req.params.scrapeId, private: false },
+    where: { id: req.params.scrapeId },
   });
   if (!scrape) {
     res.status(404).json({ message: "Collection not found" });
