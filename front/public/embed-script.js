c3e84ebcd1851fde61a217c840129ddac51e8cb2
Pramod <pramodkumar.damam73@gmail.com>
2025-10-10 20:53:10 +0530

    Reduce sidepanel default width

--- front/public/embed-script.js
+++ front/public/embed-script.js
@@ -294,7 +294,7 @@ class CrawlChatEmbed {
     resize.classList.add("crawlchat-sidepanel-resize");
 
     const handleMouseMove = (e) => {
-      const width = Math.max(Math.min(window.innerWidth - e.clientX, 500), 400);
+      const width = Math.max(Math.min(window.innerWidth - e.clientX, 500), 320);
       document.documentElement.style.setProperty(
         "--crawlchat-sidepanel-width",
         `${width}px`
