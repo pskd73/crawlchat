a6592c22c06692037cdae5cfa8096d9e73301932
Pramod Kumar <pramodkumar.damam73@gmail.com>
2026-02-25 09:42:04 +0530

    Remove dashboard tracking (#575)

--- front/app/summary/page.tsx
+++ front/app/summary/page.tsx
@@ -39,7 +39,6 @@ import moment from "moment";
 import cn from "@meltdownjs/cn";
 import { makeMeta } from "~/meta";
 import { dodoGateway } from "~/payment/gateway-dodo";
-import { track } from "~/components/track";
 import { getMessagesSummary } from "~/messages-summary";
 import { UniqueUsers } from "./unique-users";
 import { calcUniqueUsers } from "./calc-unique-users";
@@ -456,10 +455,6 @@ export default function DashboardPage({ loaderData }: Route.ComponentProps) {
       .slice(0, 20)
       .map(([title, d]) => ({ title, count: d.count }));
   }, [loaderData.messagesSummary.tags, tagsOrder]);
-
-  useEffect(() => {
-    track("dashboard", {});
-  }, []);
 
   useEffect(() => {
     if (containerRef.current) {
